cc88c5a46d8b0fbdb829ab4275312a41818928d3
ErikSando <99470600+ErikSando@users.noreply.github.com>
2025-04-25 11:34:59 +0930

    Added option to play either side, and option to play again
    The user can pick a side to play at the start, and can play again without re-opening the executable. After testing, the bot can play perfectly as both crosses or circles.
    I also added "-static-libgcc -static-libstdc++" to the makefile to prevent an error when I tried to run the executable files, not sure if this can cause other problems but at least the program runs.

--- src/Main.cpp
+++ src/Main.cpp
@@ -13,59 +13,86 @@ int main(int argc, char* argv[]) {
 
     //bool s = grid.SetGrid(crosses, circles);
 
-    grid.Print();
-
     std::string input;
 
     for (;;) {
+        bool valid_input = false;
+
+        for (;;) {
+            input = "";
+            std::cout << "Enter side (0 for crosses, 1 for circles): ";
+            std::cin >> input;
+
+            if (input == "0") break;
+
+            if (input == "1") {
+                std::cout << "Computer is making a move" << std::endl;
+
+                int move = Search(grid);
+                grid.MakeMove(move);
+
+                break;
+            }
+        }
+
+        grid.Print();
+
+        for (;;) {
+            input = "";
+            std::cout << "Enter square: ";
+            std::cin >> input;
+            //std::cout << input << std::endl;
+            
+            int user_move = -1;
+
+            try {
+                user_move = std::stoi(input);
+            }
+            catch (...) {
+                std::cout << "Invald input" << std::endl;
+                continue;
+            }
+
+            if (user_move < 0 || user_move > 8) {
+                std::cout << "Invalid square" << std::endl;
+                continue;
+            }
+
+            grid.MakeMove(user_move);
+            grid.Print();
+
+            if (grid.CheckVictory()) {
+                std::cout << "You win!" << std::endl;
+                break;
+            }
+            
+            if (grid.IsFilled()) {
+                std::cout << "Draw." << std::endl;
+                break;
+            }
+
+            std::cout << "Computer is making a move" << std::endl;
+
+            int move = Search(grid);
+            grid.MakeMove(move);
+            grid.Print();
+
+            if (grid.CheckVictory()) {
+                std::cout << "Computer wins." << std::endl;
+                break;
+            }
+            
+            if (grid.IsFilled()) {
+                std::cout << "Draw." << std::endl;
+                break;
+            }
+        }
+
         input = "";
-        std::cout << "Enter square: ";
+        std::cout << "Play again? (y/n): ";
         std::cin >> input;
-        //std::cout << input << std::endl;
-        
-        int user_move = -1;
 
-        try {
-            user_move = std::stoi(input);
-        }
-        catch (...) {
-            std::cout << "Invald input" << std::endl;
-            continue;
-        }
-
-        if (user_move < 0 || user_move > 8) {
-            std::cout << "Invalid square" << std::endl;
-            continue;
-        }
-
-        grid.MakeMove(user_move);
-        grid.Print();
-
-        if (grid.CheckVictory()) {
-            std::cout << "You win!" << std::endl;
-            break;
-        }
-        
-        if (grid.IsFilled()) {
-            std::cout << "Draw." << std::endl;
-            break;
-        }
-
-        std::cout << "Computer is making a move" << std::endl;
-
-        int move = Search(grid);
-        grid.MakeMove(move);
-        grid.Print();
-
-        if (grid.CheckVictory()) {
-            std::cout << "Computer wins." << std::endl;
-            break;
-        }
-        
-        if (grid.IsFilled()) {
-            std::cout << "Draw." << std::endl;
-            break;
-        }
+        if (input != "y" || input != "Y") break;
     }
 
     return 0;
